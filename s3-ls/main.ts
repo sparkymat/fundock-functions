d9f11c7ea4f04a49bb0d6d0ff2bb133ae2f84863
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2023-01-25 00:27:09 +1100

    Switched to working Deno aws library

--- s3-ls/main.ts
+++ s3-ls/main.ts
@@ -1,28 +1,30 @@
-import { S3Client, ListBucketsCommand } from "npm:@aws-sdk/client-s3";
+import { ApiFactory } from "https://deno.land/x/aws_api@v0.7.0/client/mod.ts";
+import { S3 } from "https://aws-api.deno.dev/v0.3/services/s3.ts";
 
-const region = Deno.env.get("AWS_REGION");
-const accessKey = Deno.env.get("AWS_ACCESS_KEY_ID");
-const secretKey = Deno.env.get("AWS_SECRET_ACCESS_KEY");
+const region = Deno.env.get("AWS_REGION") || "";
+const awsAccessKeyId = Deno.env.get("AWS_ACCESS_KEY_ID") || "";
+const awsSecretKey = Deno.env.get("AWS_SECRET_ACCESS_KEY") || "";
 
-if (!region || !accessKey || !secretKey) {
+if (!region || !awsAccessKeyId || !awsSecretKey) {
   const output = { error: "Invalid configuration" };
   console.log(JSON.stringify(output));
   Deno.exit(0);
 }
 
-const client = new S3Client({ region: "REGION" });
+// with credentials in ~/.aws/credentials:
+const s3 = new ApiFactory({
+  region,
+  credentials: { awsAccessKeyId, awsSecretKey },
+}).makeNew(S3);
 
-const command = new ListBucketsCommand({});
-
-client
-  .send(command)
+s3.listBuckets()
   .then((data) => {
     const output = {
       buckets: (data.Buckets || []).map((b) => b.Name),
     };
     console.log(JSON.stringify(output));
   })
-  .catch((error) => {
-    const output = { error: error };
+  .catch((err) => {
+    const output = { error: err };
     console.log(JSON.stringify(output));
   });
